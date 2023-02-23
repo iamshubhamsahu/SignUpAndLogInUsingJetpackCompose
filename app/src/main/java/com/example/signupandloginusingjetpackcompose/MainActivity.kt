6ad192fe68bf1316bc89b494ab7416c7fa8f9629
Shubham Sahu <iamshubhamsahufromvaranasi@gmail.com>
2023-02-23 12:19:02 +0530

    Add SignUp createUserWithEmailAndPassword(emailId, password)

--- app/src/main/java/com/example/signupandloginusingjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/signupandloginusingjetpackcompose/MainActivity.kt
@@ -1,31 +1,32 @@
 package com.example.signupandloginusingjetpackcompose
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.signupandloginusingjetpackcompose.ui.theme.SignUpAndLogInUsingJetpackComposeTheme
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity() : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -33,7 +34,10 @@ class MainActivity : ComponentActivity() {
 
         }
     }
+
+
 }
+
 
 @Composable
 fun EditText() {
@@ -46,11 +50,8 @@ fun EditText() {
     var email_signup by remember { mutableStateOf("") }
     var password_signup by remember { mutableStateOf("") }
 
-
     var enabled by remember { mutableStateOf(true) }
 
-
-    var context = LocalContext.current
 
 
     Column(
@@ -59,10 +60,10 @@ fun EditText() {
             .fillMaxWidth()
             .padding(20.dp)
     ) {
+        Text(text = "SignIn", style = TextStyle(fontSize = 20.sp))
 
-        Text(text = "SignIn",style = TextStyle(fontSize = 20.sp))
-
-        OutlinedTextField(value = email_signin,
+        OutlinedTextField(
+            value = email_signin,
             singleLine = true,
             onValueChange = { email_signin = it },
             label = { Text("Email") },
@@ -75,12 +76,11 @@ fun EditText() {
         Button(onClick = {
 
 
-
         }) {
             Text(text = "Sign In")
         }
 
-        Text(text = "Create new account",style = TextStyle(fontSize = 20.sp))
+        Text(text = "Create new account", style = TextStyle(fontSize = 20.sp))
 
         OutlinedTextField(value = name,
             singleLine = true,
@@ -98,15 +98,18 @@ fun EditText() {
             onValueChange = { email_signup = it },
             label = { Text("Email") })
 
-        OutlinedTextField(value = password_signup ,
+        OutlinedTextField(value = password_signup,
             singleLine = true,
-            onValueChange ={password_signup = it},
-            label = { Text("Password")})
+            onValueChange = { password_signup = it },
+            label = { Text("Password") })
 
 
         Button(onClick = {
 
 
+            CoroutineScope(Dispatchers.IO).launch {
+                signUpWithEmailAndPassword(FirebaseAuth.getInstance(), email_signup, password_signup)
+            }
 
         }) {
             Text(text = "Create new account ")
@@ -150,11 +153,28 @@ fun EditText() {
     }
 }
 
+
 @Composable
 fun ButtonSendMassage(action: () -> Unit) {
-    Button(onClick = {
-        action()
-    }) {
+    Button(onClick = {}) {
         Text(text = "Log in")
     }
+}
+
+
+suspend fun signUpWithEmailAndPassword(
+    firebaseAuth: FirebaseAuth, emailId: String, password: String
+): AuthResult? {
+    return try {
+        val result = firebaseAuth.createUserWithEmailAndPassword(emailId, password).await()
+       // Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+        Log.e("AuthResult", "${result.user?.email}")
+        result
+
+    } catch (e: Exception) {
+        Log.e("AuthResult", "${e.message}")
+        null
+    }
+
+
 }
